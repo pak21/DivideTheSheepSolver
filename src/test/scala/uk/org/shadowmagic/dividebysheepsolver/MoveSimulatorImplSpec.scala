0debf6d618827448b5fdf8fad5eaa2b45759d3f9
Philip Kendall <philip@shadowmagic.org.uk>
2017-05-16 21:55:40 +0100

    Add failing tests for hungry wolf moves.

--- src/test/scala/uk/org/shadowmagic/dividebysheepsolver/MoveSimulatorImplSpec.scala
+++ src/test/scala/uk/org/shadowmagic/dividebysheepsolver/MoveSimulatorImplSpec.scala
@@ -46,4 +46,46 @@ class MoveSimulatorImplSpec extends FlatSpec with MustMatchers {
     after.islands(0).sheep must be (0)
     after.islands(1).sheep must be (3)
   }
+
+  it should "be able to move hungry wolves" in {
+    // Arrange
+    val island0 = Island(3, hungryWolves = 3)
+    val island1 = Island(3)
+    val before = Level(Array(island0, island1), Seq.empty[Raft])
+
+    // Act
+    val after = moveSimulator.move(before, 0, 1)
+
+    // Assert
+    after.islands(0).hungryWolves must be (0)
+    after.islands(1).hungryWolves must be (3)
+  }
+
+  it should "keep existing hungry wolves on target island" in {
+    // Arrange
+    val island0 = Island(3, hungryWolves = 1)
+    val island1 = Island(3, hungryWolves = 1)
+    val before = Level(Array(island0, island1), Seq.empty[Raft])
+
+    // Act
+    val after = moveSimulator.move(before, 0, 1)
+
+    // Assert
+    after.islands(0).hungryWolves must be (0)
+    after.islands(1).hungryWolves must be (2)
+  }
+
+  it should "discard excess hungry wolves" in {
+    // Arrange
+    val island0 = Island(3, hungryWolves = 3)
+    val island1 = Island(3, hungryWolves = 2)
+    val before = Level(Array(island0, island1), Seq.empty[Raft])
+
+    // Act
+    val after = moveSimulator.move(before, 0, 1)
+
+    // Assert
+    after.islands(0).hungryWolves must be (0)
+    after.islands(1).hungryWolves must be (3)
+  }
 }
